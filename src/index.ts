a7b9ad65acc9a5ee8a70c2b92d0a4dbcd0460dfa
kuznecov_as <aleksandr.kuznetcov@coolclever.ru>
2019-11-14 12:13:03 +0300

    ci

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ var so: iio.ServerOptions
 
 import { Querier } from './queryier';
 import { appendFile, WriteFileOptions } from 'fs';
+import { hostname } from 'os';
 const HISTORY_PATH = "sql_history.log";
 //const q = new Querier()
-
-let li = s.listen(3002).on('connect', (socket: iio.Socket) => {
+var PORT = process.env.PORT || 3002
+let li = s.listen(PORT).on('connect', (socket: iio.Socket) => {
 
   socket.on("message", data => {
     // console.log("data: ", data)
@@ -27,8 +28,15 @@ let li = s.listen(3002).on('connect', (socket: iio.Socket) => {
 })
 
 li.on("connection", socket => {
-  //console.log(socket)
+  console.log('connection')
 })
 li.on("message", data => {
-  //console.log("li_data: ", data)
+  console.log("li_data: ", data)
 })
+console.log(`WebSockets started on ${hostname()}:${PORT}`)
+if (process.argv.includes('stop')) {
+  console.log(`args includes `stop`. Stoping`)
+  li.removeAllListeners()
+  li.server.close()
+  process.exit(0)
+}
