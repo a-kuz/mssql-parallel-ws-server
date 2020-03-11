8542a69bf7ca9ab9a279d70b9a9699b23d9af899
kuznecov_as <aleksandr.kuznetcov@coolclever.ru>
2020-03-11 18:26:23 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,77 @@
 import * as iio from 'socket.io'
 var s: iio.Server = require('socket.io')
-var so: iio.ServerOptions
-
 import { Querier } from './queryier';
-import { appendFile, WriteFileOptions } from 'fs';
+import { appendFile, WriteFileOptions, readFile, readFileSync } from 'fs';
 import { hostname } from 'os';
 const HISTORY_PATH = "sql_history.log";
-//const q = new Querier()
+`   1   3`
+
 var PORT = process.env.PORT || 3002
-let li = s.listen(PORT).on('connect', (socket: iio.Socket) => {
+let li = s.listen(PORT)
+    .on('connect', (socket: iio.Socket) => {
 
-  socket.on("message", async data => {
-    // console.log("data: ", data)
-    let writeOpts: WriteFileOptions = {}
-    appendFile(HISTORY_PATH,
-      data + String.fromCharCode(10) +
-      String.fromCharCode(1) +
-      String.fromCharCode(2) +
-      String.fromCharCode(3), err => { })
-    var q = new Querier(data)
-    socket.emit("start", {totalInstances:233})
-    q.setSocket(socket)
-    q.execQueries();
+        socket.on("message", async data => {
+            // console.log("data: ", data)        
+            appendFile(HISTORY_PATH,
+                String.fromCharCode(1) +
+                String.fromCharCode(2) +
+                Date() +
+                String.fromCharCode(10) +
+                String.fromCharCode(3) +
+                data + String.fromCharCode(10)
+                , err => { })
+            var q = new Querier(data)
+            socket.emit("start", { totalInstances: 252 })
+            q.setSocket(socket)
+            q.execQueries();
 
 
-  })
-  
-})
+        }).on('history', async data => {
+            var history: string[] = []
+            var content = (readFileSync('sql_history.log')).toString()
+            var sp: string[] = content.split(String.fromCharCode(10) +
+                String.fromCharCode(3))
+
+            var t: string
+            var sep = String.fromCharCode(1) + String.fromCharCode(2)
+            sp = sp.slice(-900)
+            for (t of sp) {
+                // console.log(t)
+                let t2 = t.split(sep)
+                history.push(t2[0])
+            }
+            let h = []
+            // tslint:disable-next-line: max-line-length
+            history = history.filter((e: string) => (e.length > 20 ? 1 : 0)).map(el => el.trim())
+            h = history.filter((el, i) => {
+                if (history.lastIndexOf(el) == i) {
+                    return 1
+                }
+            }).map(e => {
+                return { text: e, full: false }
+            })
+            socket.emit('history', h.reverse())
+        })
+
+    })
+
+
 
 li.on("connection", socket => {
-  console.log('connection')
+    console.log('connection ', socket.handshake.address)
+
+    socket.emit("serverList", Querier.servers)
 })
 li.on("message", data => {
-  console.log("li_data: ", data)
+    console.log("li_data: ", data)
+}).on("serverList", async data => {
+    console.log("serverList", Querier.servers)
+
 })
 console.log(`WebSockets started on ${hostname()}:${PORT}`)
 if (process.argv.includes('stop')) {
-  console.log(`args includes ''stop''. Stoping`)
-  li.removeAllListeners()
-  li.server.close()
-  process.exit(0)
+    console.log(`args includes ''stop''. Stoping`)
+    li.removeAllListeners()
+    li.server.close()
+    process.exit(0)
 }
--- src/queryier.ts
+++ src/queryier.ts
@@ -1,140 +1,115 @@
 import * as util from 'util';
 import { readFileSync, appendFileSync } from 'fs';
 import { ConnectionPool, IRecordSet, IResult, IRow } from 'mssql'
-import { config } from 'mssql'
+import { config, IOptions } from 'mssql'
 import * as mssql from 'mssql'
 import * as ws from 'socket.io'
 import { type } from 'os';
-
 var perf_last: number = Date.now();
-
 export class Querier {
-    constructor(strQ: string) {
-
+    constructor(strQ: string = "") {
         let serversJSON = readFileSync('config\/servers.json').toString()
-
-        this.servers = JSON.parse(serversJSON).sort(() => Math.random() > 0.5 ? 0 : -1)
+        Querier.servers = JSON.parse(serversJSON).sort(() => Math.random() > 0.5 ? 0 : -1)
+        //JSON.parse(readFileSync('config\/servers.json').toString()).sort(() => Math.random() > 0.5 ? 0 : -1)
         // this.resultTable = []
         this.strQ = strQ
-
     }
     private doned: number = 0
     private fullDoned: number = 0
-    private servers: any[]
+    static servers: any[] = JSON.parse(
+        readFileSync('config\/servers.json').toString()
+    ).sort(() => Math.random() > 0.5 ? 0 : -1)
     private TsqlQuery: string
-    private td: TextDecoder = new TextDecoder
-
+    
     public set strQ(v: string) {
         this.TsqlQuery = v;
     }
-
     public get strQ(): string {
         return this.TsqlQuery;
     }
-
-
     // private resultTable: any[] = []
     private socket: ws.Socket
-
     public setSocket(socket: ws.Socket) {
         this.socket = socket;
-        socket.emit("start", { totalInstances: (this.servers.length) })
+        socket.emit("start", { totalInstances: (Querier.servers.length) })
     }
     async executeSQL(connectionString: config): Promise<IResult<IRow>> {
         // try {
         let pool: ConnectionPool
         let conn: ConnectionPool
         var result: IResult<any>
-
-
         const q: TemplateStringsArray = this.strQ as any
-        try {
-            var conf = {}
-            
-            pool = new ConnectionPool(connectionString)
-            conn = await pool.connect()
-            result = await conn.query(q)
-            pool.close()
-        } catch (e) {
-            console.error(e)
-            return e
-        }
+        pool = await new ConnectionPool(connectionString)
+        conn = await pool.connect()
+        result = await conn.query(q)
+        await pool.close()
         return result
     }
 
+    async processingCallback(server, i: number)  {
+        console.log(server)
+        //this.doned = this.doned + 1
+        let j = 0
+        let ob: any
+        let q = this.strQ
+        var result
+        var error
+        try {
+            let o: IOptions = { connectTimeout: 3900, maxRetriesOnTransientErrors: 1, "requestTimeout": 500000, "trustedConnection": false }
+            let c: config = {
+                database: 'sup_kkm',
+                server: server.insance,
+                connectionTimeout: 3900, user: 'sa', password: 'ser09l', requestTimeout: 500000, options: o
+            }
+            result =  (await this.executeSQL(c)).recordset
+        }
+        catch (err) {
+            error = err
+            console.groupCollapsed(`err: ${err}`);
+            console.log(server.code);
+            console.groupEnd();
+            ob = [{ ib: server.insance, err }]
+            return ob
+        } finally {
+            const recordset: IRecordSet<any> | Error = result
+            // if (this.resultTable.length === 0) {
+            //     this.resultTable = [recordset]
+            // }
+            if (recordset == undefined) { ob = [{ id: -1, ib: server.code }] } else {
+                ob = (recordset as IRecordSet<any>).map((el) => {
+                    j++
+                    el.ib = server.code
+                    let ib_host: string = server.host.replace(/ost/, 'serv')
+                    el.ib_href = `http://${ib_host}:85/sup_kkm`
+                    el.id = ((i * 10000) + j)
+                    return el
+                })
+            }
+            // this.resultTable = this.resultTable.concat([...ob])
+            return ob
+        }
+    }
     async processing(): Promise<any> {
-        this.doned = 0
+        //this.doned = 0
         console.log('processing')
         var j: number
-
-        return this.servers.map(async (server, i: number) => {
-            this.doned = this.doned + 1
-            j = 0
-            let ob: any
-            let q = this.strQ
-            var result
-            try {
-                let c: config = {
-                    database: 'sup_kkm',
-                    server: server.СерверSQL,
-                    connectionTimeout: 2000, user: 'sa', password: 'ser09l'
-                }
-                result = await this.executeSQL(c)
-            }
-            catch (err) {
-                ob = [{ ib: server.СерверSQL, err }]
-                // this.resultTable = this.resultTable.concat([...ob])
-
-            } finally {
-                const recordset: IRecordSet<any> | Error = result.recordset
-                // if (this.resultTable.length === 0) {
-                //     this.resultTable = [recordset]
-                // }
-                if (recordset == undefined) { ob = [{ id: -1, ib: server.Код }] } else {
-                    ob = (recordset as IRecordSet<any>).map((el) => {
-                        j++
-                        el.ib = server.Код
-                        el.id = ((i * 10000) + j)
-                        return el
-                    })
-                }
-                // this.resultTable = this.resultTable.concat([...ob])
-
-                return ob
-            }
+        return Querier.servers.map((e,i)=>{
+            return this.processingCallback(e,i)
 
         })
     }
-
-
     async execQueries() {
-
         var f: Promise<any>[]
-
-        // try {
-        //     f = await 
-        // } catch (error) {
-        //     console.error('[38;2;255;; ERROR')
-        //     console.error(error)
-        //     if (this.socket) {
-        //         this.socket.error(error)
-        //         this.socket.emit('end')
-        //     }
-        //     return
-        // }
-
-
-
+        
         for await (const obj of (await this.processing())) {
-
+            
             let str = ''
             try {
-
                 if (obj.length) {
                     obj.forEach(element => {
                         Object.keys(element).forEach(key => {
                             if (Buffer.isBuffer(element[key])) {
-                                element[key] = buf2hex(element[key]);    
+                                element[key] = buf2hex(element[key]);
                             }
                         });
                     });
@@ -143,19 +118,16 @@ export class Querier {
                         console.timeStamp(obj[0].ib)
                         this.fullDoned++
                         // tslint:disable-next-line: max-line-length
-                        this.socket.emit("progress", { value: (this.fullDoned / this.servers.length * 100), bufferValue: (this.doned / this.servers.length * 100) })
-                        console.log(`${perf_last - Date.now()} ${this.fullDoned} / ${this.servers.length}`)
+                        this.socket.emit("progress", { value: (this.fullDoned / Querier.servers.length * 100), bufferValue: (this.doned / Querier.servers.length * 100) })
+                        console.log(`${perf_last - Date.now()} ${this.fullDoned} / ${Querier.servers.length} (${this.doned})`)
                         perf_last = Date.now()
                     }
                 }
-
             } catch (e) {
                 console.error(e)
                 console.log(obj)
             }
         }
-      
-
         if (this.socket) {
             // this.socket.send(JSON.stringify(this.resultTable))
             this.socket.emit('end')
@@ -164,4 +136,4 @@ export class Querier {
 }
 function buf2hex(buffer) { // buffer is an ArrayBuffer
     return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  }
+}
